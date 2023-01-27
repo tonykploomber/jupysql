ea3a7856dd8d26ce6e37b483be6b00aadb27fa6f
Tony Kuo <yen.c@ploomber.io>
2023-01-26 23:23:16 -0500

    Refract: share mock_log_api as fixture

--- src/tests/test_telemetry.py
+++ src/tests/test_telemetry.py
@@ -1,12 +1,7 @@
 from pathlib import Path
-# from ploomber_core.telemetry import
-# from ploomber_core.telemetry.telemetry import Telemetry
-
-
 from unittest.mock import Mock
 import pytest
 import urllib.request
-# this requires duckdb: pip install duckdb
 import duckdb
 from sql.telemetry import telemetry
 from sql import plot
@@ -21,21 +16,19 @@ def simple_db_conn():
     conn = duckdb.connect(database=":memory:")
     return conn
 
-def test_boxplot_telemetry(monkeypatch, simple_db_conn):
-
+@pytest.fixture
+def mock_log_api(monkeypatch):
     mock_log_api = Mock()
     monkeypatch.setattr(telemetry, 'log_api', mock_log_api)
+    yield mock_log_api
 
+def test_boxplot_telemetry(mock_log_api, simple_db_conn):
     # Test the injected log_api gets called  
     plot.boxplot("iris.csv", "petal width", conn=simple_db_conn)
 
     mock_log_api.assert_called()
 
-def test_histogram_telemetry(monkeypatch, simple_db_conn):
-
-    mock_log_api = Mock()
-    monkeypatch.setattr(telemetry, 'log_api', mock_log_api)
-
+def test_histogram_telemetry(mock_log_api, simple_db_conn):
     # Test the injected log_api gets called  
     plot.histogram("iris.csv", "petal width", bins=50, conn=simple_db_conn)
 
