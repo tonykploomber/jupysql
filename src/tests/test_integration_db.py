13d6eb7a3f78189b30c1ae0c5a32a083c749ee00
Tony Kuo <yen.c@ploomber.io>
2023-02-02 15:40:50 -0500

    update postgreSQL

--- src/tests/test_integration_db.py
+++ src/tests/test_integration_db.py
@@ -11,21 +11,35 @@ postgreSQLConfig = {
 
 @pytest.fixture(scope="session")
 def setup_postgreSQL():
-    df = pd.read_parquet(
-        "https://d37ci6vzurychx.cloudfront.net/\
-        trip-data/yellow_tripdata_2021-01.parquet"
-    )
     engine = create_engine(
         "postgresql://ploomber_app:ploomber_app_password@localhost/db"
     )
-    df.to_sql(name="taxi", con=engine, chunksize=100_000)
 
     yield engine
     engine.dispose()
 
 
+@pytest.fixture
+def taxi_data(setup_postgreSQL, delete_table):
+    table_name = "taxi"
+    engine = setup_postgreSQL
+    df = pd.read_parquet(
+        "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-01.parquet"
+    )
+    df.to_sql(name=table_name, con=engine, chunksize=100_000, if_exists="replace")
+    yield table_name
+    # delete_table(table_name)
+
+
+# @pytest.fixture
+# def delete_table(setup_postgreSQL, table_name):
+#     conn = setup_postgreSQL.connect()
+#     conn.execute("DROP TABLE " + table_name)
+#     conn.close()
+
+
 @pytest.mark.integtest
-def test_postgreSQL_query(ip):
+def test_postgreSQL_query(ip, taxi_data):
     ip.run_cell("%sql postgresql://ploomber_app:ploomber_app_password@localhost/db")
-    out = ip.run_cell("%sqlcmd tables")
+    out = ip.run_cell("%sql SELECT * FROM " + taxi_data + " LIMIT 3")
     print(out)
