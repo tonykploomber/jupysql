edc5606394ed9a288644c967441412d2982c888b
Tony Kuo <yen.c@ploomber.io>
2023-03-22 11:27:57 -0400

    fix: lint

--- src/tests/integration/test_containers.py
+++ src/tests/integration/test_containers.py
@@ -1,14 +1,10 @@
-from contextlib import contextmanager
-from multiprocessing import Process
 import os
-import signal
-import threading
-import time
 from sql import _testing
-from signal import pthread_kill, SIGTSTP, SIGINT
+
 is_on_github = False
 if "GITHUB_ACTIONS" in os.environ:
     is_on_github = True
+
 
 def test_postgres_container():
     with _testing.postgres(is_bypass_init=is_on_github) as container:
