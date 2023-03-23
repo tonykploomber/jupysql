1aab2bb74adda921fc7d00693a9c0a47151087c0
Tony Kuo <yen.c@ploomber.io>
2023-03-23 12:43:51 -0400

    Add: DatabaseConfigHelper test cases

--- src/tests/integration/test_containers.py
+++ src/tests/integration/test_containers.py
@@ -30,3 +30,41 @@ def test_invidual_container(
             for line in container.logs(stream=True)
         )
         assert _testing.database_ready(database=configKey)
+
+
+def test_database_config_helper(monkeypatch):
+    mock_tmp_dir = "some_folder"
+    mock_config_key = "someDatabaseKey"
+    mock_config_dict = {
+        "drivername": "some_driver_name",
+        "username": "some_username",
+        "password": "some_password",
+        "database": "some_db",
+        "host": "some_host",
+        "port": "1234",
+        "alias": "some_alias",
+        "docker_ct": {
+            "name": "some_name",
+            "image": "some_image",
+            "ports": {1234: 5678},
+        },
+    }
+    monkeypatch.setattr(
+        _testing,
+        "databaseConfig",
+        {
+            mock_config_key: mock_config_dict,
+        },
+    )
+
+    monkeypatch.setattr(_testing, "TMP_DIR", "some_folder")
+
+    assert (
+        _testing.DatabaseConfigHelper.get_database_config(mock_config_key)
+        == mock_config_dict
+    )
+    assert (
+        _testing.DatabaseConfigHelper.get_database_url(mock_config_key)
+        == "some_driver_name://some_username:some_password@some_host:1234/some_db"
+    )
+    assert _testing.DatabaseConfigHelper.get_tmp_dir() == mock_tmp_dir
