11460bbbc362357151ad2be20f44beee4fcd0bef
Tony Kuo <yen.c@ploomber.io>
2023-04-24 13:58:36 +0800

    fix: test_command

--- src/tests/test_command.py
+++ src/tests/test_command.py
@@ -130,6 +130,7 @@ def test_args(ip, sql_magic):
         "creator": None,
         "section": None,
         "persist": False,
+        'persist_replace': False,
         "no_index": False,
         "append": False,
         "connection_arguments": None,
